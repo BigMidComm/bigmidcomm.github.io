a473a98d6877c6b31781e97524cb0c1ed5bd616b
aanubhavv <anubhavg60@gmail.com>
2024-09-21 22:13:50 +0530

    made the webpage responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,14 +17,14 @@ export default function Home() {
         </div>
       </header>
 
-      <main className="flex-1 p-10 flex justify-between items-center space-x-10">
+      <main className="flex-1 p-10 flex flex-col md:flex-row justify-between items-center space-y-10 md:space-y-0 md:space-x-10">
         {/* Video Section */}
-        <div className="w-2/3"> {/* Reduced width for the video container */}
+        <div className="w-full md:w-2/3"> {/* Full width on small screens, 2/3 on larger screens */}
           <video className="w-full h-auto" autoPlay muted loop src="/video.mp4"></video>
         </div>
 
         {/* Text Section */}
-        <div className="w-1/2 text-white p-15"> {/* Increased width for the text container */}
+        <div className="w-full md:w-1/2 text-white"> {/* Full width on small screens, 1/2 on larger screens */}
           <p className="text-2xl mb-5">
             At BigMid, we are redefining innovation with a focus on solving real-world challenges through technology. Our mission is to create forward-thinking solutions that drive progress and transform industries. We believe in the power of disruptive ideas and are dedicated to pushing boundaries, delivering excellence, and making a lasting impact.
           </p>
